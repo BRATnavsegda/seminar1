05d4acacf99d83d224442bfb0a22928486021aeb
«Roman» <eremik8@yandex.ru>
2022-08-18 22:50:55 +0300

    Welcome

---
+++ no1/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.Write("Enter number:");
+int weekNumber = Convert.ToInt32(Console.ReadLine());
+if (weekNumber >=1 && weekNumber <=7)
+{
+else if (weekNumber == 1)
+{
+    System.Console.WriteLine("Monday");
+}
+else if (weekNumber == 2)
+{
+        System.Console.WriteLine("Tuesday"); 
+}
+else if (weekNumber == 3)
+{
+        System.Console.WriteLine("Wednesday"); 
+}
+else if (weekNumber == 4)
+{
+        System.Console.WriteLine("Thuesday"); 
+}
+else if (weekNumber == 5)
+{
+        System.Console.WriteLine("Friday"); 
+}
+else if (weekNumber == 6)
+{
+        System.Console.WriteLine("Saturday"); 
+}
+else (weekNumber == 7)
+{
+        System.Console.WriteLine("Sunday"); 
+}
+}
+else
+// System.Console.WriteLine("square number: " + value);
+switch(weekNumber)
+{
+case 1: System.Console.WriteLine("Monday"); break
+case 2: System.Console.WriteLine("Tuesday"); break
+default: 
+
+}
+
+// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+// Итерация №2
+
+// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+// Console.Write("Enter number : ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int number2 = - number;
+// int number2 = number * (-1);
+// while(number2 <= number){
+
+// // Console.Write($"{number2},");
+// Console.Write(number2 + ",");
+
+// ++number2;
+
+// }
