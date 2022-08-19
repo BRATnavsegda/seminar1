fe581c53d5cbad5cea7039ea2008e4f0dd835f9f
«Roman» <eremik8@yandex.ru>
2022-08-19 14:16:27 +0300

    added code from lection

--- no1/Program.cs
+++ no1/Program.cs
@@ -1,45 +1,82 @@
-﻿Console.Write("Enter number:");
-int weekNumber = Convert.ToInt32(Console.ReadLine());
-if (weekNumber >=1 && weekNumber <=7)
-{
-else if (weekNumber == 1)
-{
-    System.Console.WriteLine("Monday");
-}
-else if (weekNumber == 2)
-{
-        System.Console.WriteLine("Tuesday"); 
-}
-else if (weekNumber == 3)
-{
-        System.Console.WriteLine("Wednesday"); 
-}
-else if (weekNumber == 4)
-{
-        System.Console.WriteLine("Thuesday"); 
-}
-else if (weekNumber == 5)
-{
-        System.Console.WriteLine("Friday"); 
-}
-else if (weekNumber == 6)
-{
-        System.Console.WriteLine("Saturday"); 
-}
-else (weekNumber == 7)
-{
-        System.Console.WriteLine("Sunday"); 
-}
-}
-else
-// System.Console.WriteLine("square number: " + value);
-switch(weekNumber)
-{
-case 1: System.Console.WriteLine("Monday"); break
-case 2: System.Console.WriteLine("Tuesday"); break
-default: 
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 4
+// Console.Write("Enter number:");
+// // convert = конвертирует string в другую переменную
+// int number = Convert.ToInt32(Console.ReadLine());
 
-}
+// int value = number * number;
+
+// System.Console.WriteLine("square number:"+value);
+
+// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+
+// System.Console.Write("Enter first number : ");
+// int squareNumber = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter last number : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(squareNumber == number * number)
+// {
+// Console.WriteLine( "Yes");
+// }
+// else
+// {
+// Console.WriteLine( "No");
+// }
+
+
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// Console.Write("Enter number : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number >= 1 && number <= 7 )
+// {
+
+// switch(number)
+// {
+// case 1: System.Console.WriteLine("Monday"); break;
+
+// case 2: System.Console.WriteLine("Tuesday"); break;
+
+// case 3: System.Console.WriteLine("Wednesday"); break;
+
+// default: System.Console.WriteLine("Bad number");  break;
+// }
+
+// if(number == 1){
+// System.Console.WriteLine("Monday");
+// }  
+// else if(number == 2){
+//  System.Console.WriteLine("Tuesday");
+// }
+// else if(number == 3){
+// System.Console.WriteLine("Wednesday");
+// }
+// else{
+//     System.Console.WriteLine("Bad number"); 
+// }
+
+// }
+// else
+// {
+// System.Console.WriteLine("Bad Number");
+// }
 
 // 5. Напишите программу, которая на вход принимает
 // одно число (N), а на выходе показывает все целые
@@ -69,3 +106,15 @@ default:
 // ++number2;
 
 // }
+
+Console.Write("Enter number : ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(((number / 100) > 0) && ((number / 1000) < 1) ){
+int lastNumber = number % 10;
+System.Console.WriteLine( "Last number : "+ lastNumber);
+}
+else{
+    System.Console.WriteLine("Bad number");
+}
