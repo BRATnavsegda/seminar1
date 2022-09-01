e955327bf2b65fd2d9d2c08cd2c3fe54826bacf2
«Roman» <eremik8@yandex.ru>
2022-09-02 01:22:52 +0300

    homework4 is done

--- homeWorkSeminar4/Program.cs
+++ homeWorkSeminar4/Program.cs
@@ -37,20 +37,43 @@
 
 // Задача 29: Напишите программу, которая задаёт массив из введенного числа
 // и выводит их на экран.  пользователь вводит число элементов в массиве
-// и минимальное, и максимальное значение . надо заполнить массив рандоными данными
+// и минимальное, и максимальное значение . надо заполнить массив рандомными данными
 // от минимального до максимального значения .
 // И важное уточнение , делать все в методе который возвращает массив.
 // 5, 1, 20 -> [1, 2, 5, 7, 19]
 // 3, 1, 33 -> [6, 1, 33]
 
-void Massiv(int num1, int num2, int num3)
+// Вот так:
+// int[] Massiv(int num1, int num2, int num3)
+// {
+//     int[] array = new int[num1];
+//     Random rand = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = rand.Next(num2, num3);
+//     Console.WriteLine($"You array: {String.Join(", ", array)}");
+//     return array;
+// }
+
+// Massiv(5,1,20);
+
+// Или вот так:
+int[] Massiv()
 {
-    int temp = num1;
-    int[] mas = new int(temp);
+    Console.WriteLine("Введите число элементов в массиве ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите минимально возможное число массива ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите максимально возможное число массива ");
+    int num3 = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[num1];
     Random rand = new Random();
-    for (int i = 0; i < mas.Length; i++)
-        mas[i] = rand.Next(num2, num3);
-    Console.WriteLine(mas);
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rand.Next(num2, num3);
+    Console.WriteLine($"You array: {String.Join(", ", array)}");
+    return array;
 }
 
-Massiv(5,1,20);
+Massiv();
