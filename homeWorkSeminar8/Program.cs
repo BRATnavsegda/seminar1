1dd3c925de0da7a52bd2a8218cadb140da856d20
«Roman» <eremik8@yandex.ru>
2022-09-15 23:15:17 +0300

    finish homework8 and added seminar9

--- homeWorkSeminar8/Program.cs
+++ homeWorkSeminar8/Program.cs
@@ -8,7 +8,7 @@
 
         Console.WriteLine("№ 54: Задайте двумерный массив. Напишите программу, \nкоторая упорядочит по убыванию элементы каждой строки двумерного массива. \n");
         Console.WriteLine("№ 56: Задайте прямоугольный двумерный массив. Напишите программу, \nкоторая будет находить строку с наименьшей суммой элементов. \n");
-        Console.WriteLine("№ 58: Задайте две матрицы. Напишите программу, \nкоторая будет находить произведение двух матриц.");
+        Console.WriteLine("№ 58: Задайте две матрицы. Напишите программу, \nкоторая будет находить произведение двух матриц. \n");
         Console.WriteLine("№ 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. \nНапишите программу, которая будет построчно выводить массив, \nдобавляя индексы каждого элемента. \n");
         Console.WriteLine("№ 62: Напишите программу, которая заполнит спирально массив 4 на 4. \n");
 
@@ -19,31 +19,31 @@
         {
             SortInDescendingOrderInRows();
         }
-//         else if (taskNumber == 56)
-//         {
-//             ;
-//         }
-//         else if (taskNumber == 58)
-//         {
-//             ;
-//         }
-//         else if (taskNumber == 60)
-//         {
-//             ;
-//         }
-//         else if (taskNumber == 62)
-//         {
-//             ;
-//         }
+        else if (taskNumber == 56)
+        {
+            RowWithSmallestSum();
+        }
+        else if (taskNumber == 58)
+        {
+            ProductTwoMatrices();
+        }
+        else if (taskNumber == 60)
+        {
+            Array3D();
+        }
+        else if (taskNumber == 62)
+        {
+            SpiralArray();
+        }
 
         else if (taskNumber == 0)
         {
             Console.WriteLine("\nАнекдот напоследок: \n\nНичто не помогает так по хозяйству, как вырубленный интернет!\n\n"); break;
         }
-//         else
-//         {
-//             Console.WriteLine("Необходимо ввести номер задачи или 0 для выхода. \n Попробуйте еще раз...");
-//         }
+        else
+        {
+            Console.WriteLine("Необходимо ввести номер задачи или 0 для выхода. \n Попробуйте еще раз...");
+        }
     }
 }
 
@@ -167,6 +167,36 @@ void SortInDescendingOrderInRows()
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+void RowWithSmallestSum()
+
+{
+    Console.WriteLine("Set a rectangular two-dimensional array, please.\n");
+    int[,] array = GetRandomArray();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    int index = 0;
+    int minsum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Sum {i + 1} row = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    Console.WriteLine($"The row with the minimum sum of elements is equal to {index + 1} ");
+}
+
 
 
 // Задача 58: Задайте две матрицы. Напишите программу, 
@@ -178,7 +208,39 @@ void SortInDescendingOrderInRows()
 // 18 20
 // 15 18
 
+void ProductTwoMatrices()
+{
+    int[,] matrix1 = new int[2, 2];
+    int[,] matrix2 = new int[2, 2];
+    int[,] result = new int[2, 2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            matrix1[i, j] = new Random().Next(1, 10);
+            matrix2[i, j] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("Matrix 1: ");
+    PrintArray(matrix1);
+    Console.WriteLine("Matrix 2: ");
+    PrintArray(matrix2);
+    // result[0,0] = matrix1[0,0]*matrix2[0,0]+matrix1[0,1]*matrix2[1,0];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                result[i, j] = result[i, j] + (matrix1[i, k] * matrix2[k, j]);
+            }
+        }
+    }
+    Console.WriteLine("The product of two matrices: ");
+    PrintArray(result);
 
+}
 
 // Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, 
@@ -189,7 +251,23 @@ void SortInDescendingOrderInRows()
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+void Array3D()
+{
+    int[,,] array = new int[2, 2, 2];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+                Console.Write($"[{i},{j},{k}] - {array[i, j, k]}; ");
+            }
+            Console.WriteLine();
+        }
 
+    }
+}
 
 
 
@@ -199,6 +277,32 @@ void SortInDescendingOrderInRows()
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+void SpiralArray()
+{
+    int size = 4;
+
+    int[,] array = new int[size, size];
+
+    int num = 1;
+    int i = 0;
+    int j = 0;
+
+    while (num <= size * size)
+    {
+        array[i, j] = num;
+        if (i <= j + 1 && i + j < size - 1)
+            ++j;
+        else if (i < j && i + j >= size - 1)
+            ++i;
+        else if (i >= j && i + j > size - 1)
+            --j;
+        else
+            --i;
+        ++num;
+    }
+    PrintArray(array);
+}
 
 
 LetsGo();
---
+++ no9/Program.cs
@@ -0,0 +1,65 @@
+﻿// Калькулятор 1
+
+Console.WriteLine("Введите через Enter первое число, оператор (+, -, *, /) и второе число:");
+double input1 = Convert.ToDouble(Console.ReadLine());
+string input2 = Console.ReadLine();
+double input3 = Convert.ToDouble(Console.ReadLine());
+
+double output = 0;
+
+if (input2 == "*") output = input1 * input3;
+if (input2 == "/") output = input1 / input3;
+if (input2 == "-") output = input1 - input3;
+if (input2 == "+") output = input1 + input3;
+
+Console.WriteLine($"{input1} {input2} {input3} = {output}");
+
+// Калькулятор 2
+
+// Ввести число, знак, второе число
+// Сохраняем число
+// если точка вместо знака, то стираем предыдущий ответ
+// с предыдущим ответом выполняем действия
+// символ q - выход из программы
+
+Console.WriteLine("Введите число");
+double result = Convert.ToInt32(Console.ReadLine());
+char sign=' ';
+
+while (true)
+{
+    Console.WriteLine("Введите знак");
+    sign = Console.ReadKey().KeyChar;
+    if (sign=='q' )
+    break;
+
+    Console.WriteLine();
+
+    Console.WriteLine("Введите число");
+    int B = Convert.ToInt32(Console.ReadLine());
+
+    switch (sign)
+    {
+        case '+':
+            result = result + B;
+            break;
+        case '-':
+            result = result - B;
+            break;
+        case '*':
+            result = result * B;
+            break;
+        case '/':
+            result = result / B;
+            break;
+        case '.':
+            
+            result = 0;
+            break;
+
+        default:
+            Console.WriteLine("Error! Wrong sign!");
+            break;
+    }
+    Console.WriteLine($"Result is {result}");
+}
